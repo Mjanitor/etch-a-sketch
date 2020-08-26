90685d263114ef76a88d2417efdb429564ae48cc
Mike <mikejanitor@gmail.com>
2020-08-26 05:20:44 -0400

    Added the bulk of the additional features

--- README.md
+++ README.md
@@ -4,6 +4,9 @@ Basic etch-a-sketch that you're familiar with.
 Move your mouse over the boxes to color them red.
 
 ###### Planned Features ######
-- Create a reset button in lieu of reloading the page
-- Allow you to change the color as you draw
-- Give me ideas . .
+- ~~Create a reset button in lieu of reloading the page~~
+- ~~Allow you to change the color as you draw~~
+- Button the wipes the canvas
+- Make a slider for the pen size
+- Clicking disables the pen
+- General UI improvements
--- etch.js
+++ etch.js
@@ -1,26 +1,74 @@
 const container = document.getElementById('container');
+const toolbar = document.getElementById('toolbar');
 
-// Define container grid styling
-container.setAttribute("style", "height: 33%; width: 33%; display: grid; grid-template-columns: repeat(16, 1fr); grid-template-rows: repeat(16, 1fr);\
-margin: 0 33%; border: 2px solid black;");
+let numCells = 16;   // The amount of Div items for sketching
+container.setAttribute("style", `grid-template-columns: repeat(${numCells}, 1fr); grid-template-rows: repeat(${numCells}, 1fr);`);
 
-let numCells = 256;   // The amount of Div items for sketching
 
-// Create an 16x16 Grid of divs
-for (let i = 0; i < numCells; i++) {
-    let cell = document.createElement('div');
-    // Assign each cell an ID
-    cell.id = "grid" + [i];
-    // Define CSS elements to apply mouseover properties
-    cell.setAttribute("style", "height: 20px;\
-    border-bottom: 1px solid black; border-right: 1px solid black;");
-    cell.onmouseover = function() {
-        cell.style.backgroundColor = "red";
-    }
-    // Attach the div to the container
-    container.appendChild(cell);
+// Create the Pen Size Button
+let size = document.createElement('button');
+size.textContent = "Change Pen Size";
+
+// Pen Size
+size.onclick = function() {
+    container.innerHTML = "";
+    let numCells = prompt("Enter a number.  The larger the number, the smaller the brush.", 16);
+    container.setAttribute("style", `grid-template-columns: repeat(${numCells}, 1fr);`);
+
+    makeGrid(numCells);
 }
 
-//TODO: Give a Border to the container
-//TODO: Create the Toolbar div
-//TODO: Create buttons for the toolbar
+// Color Selector
+let color = document.createElement("input");
+color.setAttribute("type", "color");
+
+// Rainbow Coloration Checkbox
+let rainbow = document.createElement("input");
+rainbow.setAttribute("type", "checkbox");
+
+// Rainbow Text
+let p = document.createElement('p');
+p.textContent = "Rainbow Mode?";
+
+toolbar.appendChild(size);
+toolbar.appendChild(color);
+toolbar.appendChild(p);
+toolbar.appendChild(rainbow);
+
+
+makeGrid(numCells);  // run the function
+
+
+// Function to create an 16x16 Grid of cells
+function makeGrid(cells) {
+    for (let i = 0; i < cells**2; i++) {
+        let cell = document.createElement('div');
+        // Assign each cell an ID
+        cell.id = "grid" + [i];
+        // Define CSS elements to apply mouseover properties
+        let count = 0;   // Alpha variable
+        cell.onmouseover = function() {
+            if (!rainbow.checked) {
+                // Pen Color is whatever color is selected
+                cell.style.backgroundColor = color.value;
+            }
+            else {
+                // Defining random coloration
+                let r = Math.floor(Math.random() * 255);
+                let g = Math.floor(Math.random() * 255);
+                let b = Math.floor(Math.random() * 255);
+
+                //Output
+                cell.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+            }
+        }
+        // Attach the div to the container
+        container.appendChild(cell);
+    }
+}
+
+
+//TODO: Create a button that wipes the canvas
+//TODO: Make a slider for the pen size that isn't counterintuitive
+//TODO: Clicking disables the pen
+//TODO: Make it not look like shit
